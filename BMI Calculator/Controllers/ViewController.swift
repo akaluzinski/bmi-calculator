8d56a8ebda8d7dbf9278ee8e8b66b00d5b706f2c
Adrian Kaluzinski <akaluzin@akamai.com>
2022-11-29 19:14:39 +0100

    Update labels on slider's change

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -1,7 +1,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        
+        heightLabel.text = String(format: "%.2f", sender.value)
+        
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(Int(sender.value))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
